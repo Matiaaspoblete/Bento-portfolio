3f5fac8d1887506bafd68dcc4acb6382d480fdab
Matias <matiaspoblete318@gmail.com>
2024-03-26 20:34:21 +0200

    commit workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Matiaaspoblete.github.io',
+  base: 'Bento-portfolio',
+  integrations: [tailwind()],
+  buildOptions: {
+    ssr: true, // Ensure SSR is enabled
+    module: true, // Ensure module support is enabled
+  }
+});
